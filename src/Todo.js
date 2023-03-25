1a7dac0976b3af8e0741c8c7837cab46cd6bc866
Melvin Mathew <vinumon2000@gmail.com>
2023-03-25 19:58:06 +0530

    deleted unnecessary files

---
+++ src/Todo.js
@@ -0,0 +1,7 @@
+export default function Todo(){
+    return(
+        <div style={{height:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
+            hello
+        </div>
+    )
+}
